ead53502d4da6fb1bada77e2be5d3b1ac1dbb058
nureddinhaji <drnhaji@hotmail.com>
2024-08-19 21:52:27 +0300

    Condense all input validation functions into one function

--- script.js
+++ script.js
@@ -3,32 +3,12 @@ const monthInput = document.querySelector("#month");
 const yearInput = document.querySelector("#year");
 const currentYear = new Date().getFullYear();
 
-function checkDay(day) {
-    if(day < 0 || day > 31 || day == "") {
-        dayInput.classList.add("card__input--error")
+function cheakInputs(input, max, dom) {
+    if(input < 0 || input > max || input == "") {
+        dom.classList.add("card__input--error")
         return false;
     } else {
-        dayInput.classList.remove("card__input--error")
-        return true;
-    }
-}
-
-function checkMonth(month) {
-    if(month < 0 || month > 12 || month == "") {
-        monthInput.classList.add("card__input--error")
-        return false;
-    } else {
-        monthInput.classList.remove("card__input--error")
-        return true;
-    }
-}
-
-function checkYear(year) {
-    if(year < 0 || year > currentYear || year == "") {
-        yearInput.classList.add("card__input--error")
-        return false;
-    } else {
-        yearInput.classList.remove("card__input--error")
+        dom.classList.remove("card__input--error")
         return true;
     }
 }
@@ -38,11 +18,11 @@ document.querySelector(".card__button").addEventListener("click", () => {
     const month = monthInput.value;
     const year = yearInput.value;
 
-    checkDay(day);
-    checkMonth(month);
-    checkYear(year);
+    const dayValidate = cheakInputs(day, 31, dayInput)
+    const monthValidate = cheakInputs(month, 12, monthInput)
+    const yearValidate = cheakInputs(year, currentYear, yearInput)
 
-    if(checkDay(day) && checkMonth(month) &&  checkYear(year)) {
+    if(dayValidate && monthValidate &&  yearValidate) {
         const birthDay = new Date(`${year}-${month}-${day}`);
     const dateNow = new Date();
 
