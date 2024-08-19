bd8789070e93d891a4fe0c434ade3631f6c29fbb
nureddinhaji <drnhaji@hotmail.com>
2024-08-19 22:05:49 +0300

    check if inputed number is integer or not in validation function

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const yearInput = document.querySelector("#year");
 const currentYear = new Date().getFullYear();
 
 function cheakInputs(input, max, dom) {
-    if(input < 0 || input > max || input == "") {
-        dom.classList.add("card__input--error")
+    if (input < 0 || input > max || input == "" || !Number.isInteger(Number(input))) {
+        dom.classList.add("card__input--error");
         return false;
     } else {
-        dom.classList.remove("card__input--error")
+        dom.classList.remove("card__input--error");
         return true;
     }
 }
@@ -18,20 +18,23 @@ document.querySelector(".card__button").addEventListener("click", () => {
     const month = monthInput.value;
     const year = yearInput.value;
 
-    const dayValidate = cheakInputs(day, 31, dayInput)
-    const monthValidate = cheakInputs(month, 12, monthInput)
-    const yearValidate = cheakInputs(year, currentYear, yearInput)
+    const dayValidate = cheakInputs(day, 31, dayInput);
+    const monthValidate = cheakInputs(month, 12, monthInput);
+    const yearValidate = cheakInputs(year, currentYear, yearInput);
 
-    if(dayValidate && monthValidate &&  yearValidate) {
+    if (dayValidate && monthValidate && yearValidate) {
         const birthDay = new Date(`${year}-${month}-${day}`);
-    const dateNow = new Date();
+        const dateNow = new Date();
 
-    const ageMS = dateNow - birthDay
+        const ageMS = dateNow - birthDay;
 
-    const msInYear = 1000 * 60 * 60 * 24 * 365.25
+        const msInYear = 1000 * 60 * 60 * 24 * 365.25;
 
-    const ageYear = ageMS / msInYear
-    
-    document.querySelector(".card__result-age").textContent = Math.round(ageYear)
+        const ageYear = ageMS / msInYear;
+
+        document.querySelector(".card__result-age").textContent =
+            Math.round(ageYear);
+    } else {
+        document.querySelector(".card__result-age").textContent = "--";
     }
-})
+});
